768d3ee4fb7c0c233208390d6f72f65d0dfeff1c
Bouhamla <chaimaabohamla229@gmail.com>
2026-01-01 17:34:48 +0100

    Modification du message de bienvenue

--- src/main/java/com/emsi/App.java
+++ src/main/java/com/emsi/App.java
@@ -2,6 +2,6 @@ package com.emsi;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+        System.out.println("Bonjour Chaimae projet  DevOps ");
     }
 }
